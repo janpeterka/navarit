30f8ed8b36bad892fed173688389c6e839ab4274
Jan Peterka <jan.peterka@hey.com>
2023-12-10 21:42:14 +0100

    Set and authenticate current_user in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::Base
+class ApplicationController < PublicApplicationController
   include Pagy::Backend
 
-  before_action :set_current_user
+  # before_action :set_current_user
+  before_action :authenticate_user!
 
-  def set_current_user
-    Current.user = if Rails.env.production?
-                     User.find_by(id: session[:user_id])
-                   elsif Rails.env.development?
-                     User.first
-                   elsif Rails.env.test?
-                     User.first || FactoryBot.create(:user)
-                   end
-  end
+  # def set_current_user
+  #   current_user
+  #   # current_user = if Rails.env.production?
+  #   #                  User.find_by(id: session[:user_id])
+  #   #                elsif Rails.env.development?
+  #   #                  User.first
+  #   #                elsif Rails.env.test?
+  #   #                  User.first || FactoryBot.create(:user)
+  #   #                end
+  # end
 end
