921a0243634633f9fd645fc4312c3c75f78d1282
Jan Peterka <jan.peterka@hey.com>
2023-12-11 17:12:08 +0100

    Use creator_id directly in tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,5 @@
 class ApplicationController < PublicApplicationController
   include Pagy::Backend
 
-  # before_action :set_current_user
   before_action :authenticate_user!
-
-  # def set_current_user
-  #   current_user
-  #   # current_user = if Rails.env.production?
-  #   #                  User.find_by(id: session[:user_id])
-  #   #                elsif Rails.env.development?
-  #   #                  User.first
-  #   #                elsif Rails.env.test?
-  #   #                  User.first || FactoryBot.create(:user)
-  #   #                end
-  # end
 end
--- test/factories/ingredients.rb
+++ test/factories/ingredients.rb
@@ -4,10 +4,12 @@ FactoryBot.define do
   factory :hummus, class: Ingredient do
     id { 1 }
     name { 'Hummus' }
+    created_by { 1 }
   end
 
   factory :carrot, class: Ingredient do
     id { 2 }
     name { 'Carrot' }
+    created_by { 1 }
   end
 end
