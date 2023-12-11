4b76f8cb010dcb43cc0af4bee7e4f83488865890
Jan Peterka <jan.peterka@hey.com>
2023-12-11 17:29:50 +0100

    Integrate devise into system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,13 +3,14 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include Devise::Test::IntegrationHelpers
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
   setup do
-    current_user = User.find_by(id: 1) || FactoryBot.create(:user)
+    sign_in User.find_by(id: 1) || FactoryBot.create(:user)
   end
 
   teardown do
-    current_user = nil
+    sign_out :user
   end
 end
