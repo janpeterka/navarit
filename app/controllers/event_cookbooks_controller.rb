dbdf7c4c3aee3e90a0ac7df657cd7520355d2809
Jan Peterka <jan.peterka@hey.com>
2023-12-13 19:16:12 +0100

    Create a simple version of cookbook using Prawn

--- app/controllers/event_cookbooks_controller.rb
+++ app/controllers/event_cookbooks_controller.rb
@@ -4,5 +4,15 @@ class EventCookbooksController < ApplicationController
   def show
     @event = Event.find(params[:event_id])
     @cookbook = EventCookbook.new(@event)
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        send_data @cookbook.pdf.render,
+                  filename: "#{@event.name}_cookbook.pdf",
+                  type: 'application/pdf',
+                  disposition: 'inline'
+      end
+    end
   end
 end
--- app/models/event_cookbook.rb
+++ app/models/event_cookbook.rb
@@ -2,11 +2,58 @@
 
 class EventCookbook
   include ActiveModel::Model
+  include DateHelper
+  include ::RecipeIngredientsHelper
 
-  attr_reader :event, :recipes, :days
+  attr_reader :event
 
   def initialize(event)
     @event = event
-    @days = (@event.date_from..@event.date_to)
+  end
+
+  def pdf # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+    document = Prawn::Document.new(margin: 30, page_size: 'A4', page_layout: :portrait)
+
+    document.font_families.update('DejaVu' => {
+                                    normal: "#{Rails.root}/fonts/dejavu/ttf/DejaVuSans.ttf",
+                                    bold: "#{Rails.root}/fonts/dejavu/ttf/DejaVuSans-Bold.ttf",
+                                    italic: "#{Rails.root}/fonts/dejavu/ttf/DejaVuSans-Oblique.ttf",
+                                    bold_italic: "#{Rails.root}/fonts/dejavu/ttf/DejaVuSans-BoldOblique.ttf"
+                                  })
+
+    document.font 'DejaVu'
+
+    @event.daily_plans.each do |day|
+      document.start_new_page unless document.page_number == 1
+
+      document.text formatted_date(day.date).to_s, size: 20, style: :bold
+
+      day.daily_plan_recipes.each do |daily_recipe|
+        recipe = daily_recipe.recipe
+        document.move_down 20
+        document.text "#{recipe.name} pro #{daily_recipe.portion_count} lidí", size: 16, style: :bold
+        document.markup recipe.description&.html_safe
+        document.move_down 10
+        document.text 'suroviny:', style: :bold
+        ingredient_data = []
+        daily_recipe.recipe.recipe_ingredients.each do |recipe_ingredient|
+          ingredient = recipe_ingredient.ingredient
+          ingredient_data << [ingredient.name,
+                              formatted_amount_with_unit(recipe_ingredient, daily_recipe.portion_count)]
+        end
+        document.table(ingredient_data, cell_style: { borders: [], padding: 2 },
+                                        column_widths: [document.bounds.width / 2, document.bounds.width / 2])
+      end
+      document.move_down 20
+    end
+
+    document.number_pages '<page> z <total>', {
+      start_count_at: 1,
+      page_filter: :all,
+      at: [document.bounds.right - 50, 0],
+      # align: :center, # not working with `at``
+      size: 12
+    }
+    document
   end
 end
