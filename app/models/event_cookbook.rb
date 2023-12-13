7ed4f5baa91a4f5964f5ff38e8060066071e1b6b
Jan Peterka <jan.peterka@hey.com>
2023-12-13 19:23:47 +0100

    Improve formatting, speed it up

--- app/models/event_cookbook.rb
+++ app/models/event_cookbook.rb
@@ -8,7 +8,7 @@ class EventCookbook
   attr_reader :event
 
   def initialize(event)
-    @event = event
+    @event = Event.includes(daily_plans: { daily_plan_recipes: :recipe }).find(event.id)
   end
 
   def pdf # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
@@ -26,12 +26,15 @@ class EventCookbook
     @event.daily_plans.each do |day|
       document.start_new_page unless document.page_number == 1
 
-      document.text formatted_date(day.date).to_s, size: 20, style: :bold
+      document.text "#{weekday_name(day.date)} #{formatted_date(day.date)}", size: 18, style: :bold
+      document.move_down 15
 
       day.daily_plan_recipes.each do |daily_recipe|
         recipe = daily_recipe.recipe
-        document.move_down 20
-        document.text "#{recipe.name} pro #{daily_recipe.portion_count} lidí", size: 16, style: :bold
+        document.text recipe.name, size: 16, style: :bold
+        document.text "(pro #{daily_recipe.portion_count.to_i} lidí)", size: 10,
+                                                                       style: :italic
+        document.move_down 7
         document.markup recipe.description&.html_safe
         document.move_down 10
         document.text 'suroviny:', style: :bold
@@ -39,20 +42,22 @@ class EventCookbook
         daily_recipe.recipe.recipe_ingredients.each do |recipe_ingredient|
           ingredient = recipe_ingredient.ingredient
           ingredient_data << [ingredient.name,
-                              formatted_amount_with_unit(recipe_ingredient, daily_recipe.portion_count)]
+                              formatted_amount_with_unit(recipe_ingredient,
+                                                         daily_recipe.portion_count)]
         end
         document.table(ingredient_data, cell_style: { borders: [], padding: 2 },
                                         column_widths: [document.bounds.width / 2, document.bounds.width / 2])
+        document.move_down 10
       end
       document.move_down 20
     end
 
-    document.number_pages '<page> z <total>', {
+    document.number_pages '<page> / <total>', {
       start_count_at: 1,
       page_filter: :all,
-      at: [document.bounds.right - 50, 0],
-      # align: :center, # not working with `at``
-      size: 12
+      at: [document.bounds.right - 30, 0],
+      align: :center,
+      size: 10
     }
     document
   end
