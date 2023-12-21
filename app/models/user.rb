ef5a8e36c6a55e6f73b8810ced4192508a33d626
Jan Peterka <jan.peterka@hey.com>
2023-12-21 13:11:04 +0100

    move omniauth logic to concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include User::Omniauthable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
@@ -16,15 +17,6 @@ class User < ApplicationRecord
   has_many :portion_types
 
   before_validation :set_legacy_columns, on: :create
-
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
-
-    # Uncomment the section below if you want users to be created if they don't exist
-    user ||= User.create(name: data['name'], email: data['email'], password: Devise.friendly_token[0, 20])
-    user
-  end
 
   def name
     full_name
