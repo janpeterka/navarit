25ca6c2edd483d49ba9a3a6de0d62ccff3befd90
Jan Peterka <jan.peterka@hey.com>
2024-01-05 17:10:12 +0100

    Add menu

--- app/components/menu_component.rb
+++ app/components/menu_component.rb
@@ -1,16 +1,13 @@
-# frozen_string_literal: true
-
 # This component renders a menu with multiple items.
 class MenuComponent < ApplicationComponent
   renders_many :items, lambda { |name = nil, path, **kwargs, &block|
-    default_classes = "border-transparent text-gray-500 whitespace-nowrap
-                         py-4 px-1 border-b-2 font-medium text-sm"
+    default_classes = 'text-gray-500 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
 
     if current_path.to_s.start_with?(path)
       default_classes.gsub!('text-gray-500', '')
-      default_classes << ' text-blue-600 border-blue-500'
+      default_classes << ' text-emerald-700 border-emerald-700'
     else
-      default_classes << ' hover:border-gray-300 hover:text-gray-700'
+      default_classes << ' border-transparent hover:border-gray-300 hover:text-gray-700'
     end
 
     if block
