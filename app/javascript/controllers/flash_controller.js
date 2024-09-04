30440e71f10f50e9a5c1c8b19927b89b2a6dc9b9
Jan Peterka <jan.peterka@hey.com>
2024-09-04 17:34:22 +0200

    Make flashes disappear, style for multiple flashes

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -2,10 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="flash"
 export default class extends Controller {
-  connect() {
-  }
-
-  hide() {
+  destroy() {
     this.element.remove()
   }
 }
