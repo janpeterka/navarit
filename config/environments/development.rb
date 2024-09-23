6b6519bfd0f79a0eae6359403cb33e90916ef108
Jan Peterka <jan.peterka@hey.com>
2024-09-23 18:37:31 +0200

    Set sending emails from Solid Errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,11 +82,11 @@ Rails.application.configure do
 
   # Configure Solid Errors
   config.solid_errors.connects_to = { database: { writing: :errors } }
-  config.solid_errors.send_emails = true
-  config.solid_errors.email_from = ""
-  config.solid_errors.email_to = ""
-  config.solid_errors.username = Rails.application.credentials.dig(:solid_errors, :username)
-  config.solid_errors.password = Rails.application.credentials.dig(:solid_errors, :password)
+  config.solid_errors.send_emails = false
+  # config.solid_errors.email_from = ""
+  # config.solid_errors.email_to = ""
+  # config.solid_errors.username = Rails.application.credentials.dig(:solid_errors, :username)
+  # config.solid_errors.password = Rails.application.credentials.dig(:solid_errors, :password)
 end
 
 Rails.application.routes.default_url_options[:host] = "navarit.cz"
