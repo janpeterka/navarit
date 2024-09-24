3ccc9d0e24f00b9d10fa13cbc18aa9ae548d2bbe
Jan Peterka <jan.peterka@hey.com>
2024-09-24 23:10:44 +0200

    Redirect admins to admin page

--- feedback/app/controllers/feedback/index_controller.rb
+++ feedback/app/controllers/feedback/index_controller.rb
@@ -1,5 +1,9 @@
 class Feedback::IndexController < Feedback::ApplicationController
   def show
+    if current_user.respond_to?(:admin?) && current_user.admin?
+      redirect_to admin_posts_url
+    end
+
     @posts = Feedback::Post.created_by(current_user)
   end
 end
