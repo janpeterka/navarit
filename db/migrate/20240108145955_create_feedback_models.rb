f794fc912a225a5d7c44132cdcf792aca3198e32
Jan Peterka <jan.peterka@hey.com>
2024-11-01 21:49:09 +0100

    Merge migrations to one

--- db/migrate/20240108145955_create_feedback_models.rb
+++ db/migrate/20240108145955_create_feedback_models.rb
@@ -3,10 +3,10 @@ class CreateFeedbackModels < ActiveRecord::Migration[7.1]
     create_table :feedback_posts do |t|
       t.text :description
       t.references :user, null: false
-      t.datetime :last_synchronized_at
+      # t.datetime :last_synchronized_at
       t.integer :status
-      t.integer :issue_id
-      t.string :issue_url
+      # t.integer :issue_id
+      # t.string :issue_url
 
       t.timestamps
     end
@@ -15,9 +15,18 @@ class CreateFeedbackModels < ActiveRecord::Migration[7.1]
       t.text :content
       t.references :post, null: false
       t.references :user
-      t.datetime :last_synchronized_at
-      t.integer :comment_id
-      t.string :comment_url
+      # t.datetime :last_synchronized_at
+      # t.integer :comment_id
+      # t.string :comment_url
+
+      t.timestamps
+    end
+
+    create_table :feedback_notifications do |t|
+      t.string :title
+      t.references :user, null: false, foreign_key: true, type: :integer
+      t.datetime :read_at
+      t.references :notifiable, polymorphic: true, null: false
 
       t.timestamps
     end
