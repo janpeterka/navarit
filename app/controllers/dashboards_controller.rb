935516445ea270af8a5fefc6bcad95f468912bad
Jan Peterka <jan.peterka@hey.com>
2025-03-21 18:08:45 +0100

    Correct selecting showcased recipes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,8 @@
 class DashboardsController < ApplicationController
   def show
     @nearest_future_event = current_user.events.future.order(:date_from).first
-    @showcased_public_recipes = Recipe.published.includes(:labels, :author, :category, :reactions).order(created_by: :desc).limit(3)
+    @showcased_public_recipes = Recipe.published.without_shopping
+                                      .includes(:labels, :author, :category, :reactions)
+                                      .order(created_at: :desc).limit(3)
   end
 end
