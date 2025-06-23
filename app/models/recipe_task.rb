1fb420b43433046e1c18493ab2084ae8132db564
Jan Peterka <jan.peterka@hey.com>
2025-06-23 23:28:30 +0200

    Require days before cooking

--- app/models/recipe_task.rb
+++ app/models/recipe_task.rb
@@ -2,4 +2,15 @@ class RecipeTask < ApplicationRecord
   belongs_to :recipe
 
   validates :name, presence: true
+  validates :days_before_cooking, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  before_validation :set_default_days
+
+  private
+
+    def set_default_days
+      return if days_before_cooking.present?
+
+      self.days_before_cooking = 0
+    end
 end
