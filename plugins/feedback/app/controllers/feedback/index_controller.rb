afaa3c6057cf4c700b56a1c2ab840e21c7a87c92
Jan Peterka <jan.peterka@hey.com>
2025-07-13 11:11:39 +0200

    Don't redirect admin to admin feedback

--- plugins/feedback/app/controllers/feedback/index_controller.rb
+++ plugins/feedback/app/controllers/feedback/index_controller.rb
@@ -1,8 +1,8 @@
 class Feedback::IndexController < Feedback::ApplicationController
   def show
-    if current_user.respond_to?(:admin?) && current_user.admin?
-      redirect_to admin_posts_url
-    end
+    # if current_user.respond_to?(:admin?) && current_user.admin?
+    #   redirect_to admin_posts_url
+    # end
 
     @posts = Feedback::Post.created_by(current_user)
   end
